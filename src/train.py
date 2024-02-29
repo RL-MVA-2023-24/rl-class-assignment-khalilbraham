cd7234abab7d503819e9f21bdb69677384fc4802
khalilbraham <khalilbrahem.kb@gmail.com>
2024-02-29 21:00:59 +0100

    First model

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 from copy import deepcopy
 import os
 import pickle 
-y
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
